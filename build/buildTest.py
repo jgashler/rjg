a6eefe4654513346abea67beb9297db36ff1dddd
Jack Gashler <jgashler44@gmail.com>
2023-09-09 16:49:39 -0600

    delete test files

--- build/buildTest.py
+++
@@ -1,5 +0,0 @@
-import maya.cmds as mc
-
-def buildSphere(name = 'hoinky'):
-    sph = mc.polySphere(n=name, ch=False)[0]
-    return sph
--- build/parts/partTest.py
+++
@@ -1,8 +0,0 @@
-import maya.cmds as mc
-import importlib
-
-import rjg.build.buildTest as buildTest
-importlib.reload(buildTest)
-
-def partTestSph(name='partHoinky'):
-    return buildTest.buildSphere(name)
