5bf877f5405e78cbf6e6e7df55ebdd0050f4ebce
Jack Gashler <jgashler44@gmail.com>
2023-09-12 14:40:17 -0600

    added fk

---
+++ build/fk.py
@@ -0,0 +1,84 @@
+import maya.cmds as mc
+from importlib import reload
+
+import rjg.build.chain as rChain
+import rjg.libs.control.ctrl as rCtrl
+reload(rChain)
+reload(rCtrl)
+
+class Fk:
+    def __init__(self, side=None, part=None, guide_list=None, gimbal=None, offset=None, pad='auto', ctrl_scale=1, remove_last=True, 
+                 add_joints=True, fk_shape='circle', gimbal_shape='circle', offset_shape='square'):
+        self.side = side
+        self.part = part
+        self.base_name = part + "_" + side
+        
+        self.guide_list = guide_list
+        self.gimbal = gimbal
+        self.offset = offset
+        self.pad = pad
+        self.ctrl_scale = ctrl_scale
+        self.remove_last = remove_last
+        self.add_joints = add_joints
+        self.fk_shape = fk_shape
+        self.gimbal_shape = gimbal_shape
+        self.offset_shape = offset_shape
+
+        if self.pad == 'auto':
+            self.pad = len(str(len(self.guide_list))) + 1
+        
+        if self.guide_list:
+            if not isinstance(self.guide_list, list):
+                self.guide_list = [self.guide_list]
+
+    def build_fk(self):
+        self.build_fk_controls()
+
+        if self.add_joints:
+            self.build_fk_chain()
+
+        if self.remove_last:
+            mc.delete(self.fk_ctrls[-1].top)
+
+    '''
+    Builds up to three layers of controls for each joint: fk, gimbal, and offset. Innermost will be the constraint target for joints.
+    '''
+    def build_fk_controls(self):
+        self.fk_ctrls = []
+        self.gim_ctrls = []
+        self.ofst_ctrls = []
+
+        for i, pose in enumerate(self.guide_list):
+            num = str(i+1).zfill(self.pad)
+            if pose == self.guide_list[0]:
+                par = None
+            fk = rCtrl(parent=par, shape=self.fk_shape, side=self.side, suffix='CTRL', name=self.part + "_" + num + "_fk", axis='y', group_type='main', 
+                       rig_type='fk', translate=pose, rotate=pose, ctrl_scale=self.ctrl_scale)
+            par = fk.ctrl
+            self.fk_ctrls.append(fk)
+
+            # output_ctrls: constraint target for joints
+            self.output_ctrls = self.fk_ctrls
+
+            if self.gimbal:
+                gim = rCtrl(parent=par, shape=self.gimbal_shape, side=self.side, suffix='CTRL', name=self.part + "_" + num + "_gimbal", axis='y', group_type='main', 
+                       rig_type='gimbal', translate=pose, rotate=pose, ctrl_scale=self.ctrl_scale*0.8)
+                par = gim.ctrl
+                self.gim_ctrls.append(gim)
+                self.output_ctrls = self.gim_ctrls
+
+            if self.offset:
+                ofst = rCtrl(parent=par, shape=self.offset_shape, side=self.side, suffix='CTRL', name=self.part + "_" + num + "_offset", axis='y', group_type='main', 
+                       rig_type='offset', translate=pose, rotate=pose, ctrl_scale=self.ctrl_scale*0.55)
+                self.ofst_ctrls.append(ofst)
+                self.output_ctrls = self.ofst_ctrls
+
+    '''
+    Places and constrains joints at each control location.
+    '''
+    def build_fk_chain(self):
+        transform_list = [oc.ctrl for oc in self.output_ctrls]
+        self.fk_chain = rChain(transform_list=transform_list, side=self.side, suffix='fk_JNT', name=self.part)
+        self.fk_chain.create_from_transforms(parent_constraint=True, scale_constraint=False)
+        self.fk_joints = self.fk_chain.joints
+
---
+++ build/parts/fk_chain.py
@@ -0,0 +1,59 @@
+import maya.cmds as mc
+from importlib import reload
+
+import rjg.build.rigModule as rModule
+import rjg.build.chain as rChain
+import rjg.build.fk as rFk
+import rjg.libs.attribute as rAttr
+reload(rModule)
+reload(rChain)
+reload(rFk)
+reload(rAttr)
+
+class fk_chain(rModule.RigModule, rFk.Fk):
+    def __init__(self, side=None, part=None, guide_list=None, gimbal=None, offset=None, pad='auto', ctrl_scale=1, remove_last=True, 
+                 fk_shape='circle', gimbal_shape='circle', offset_shape='square', model_path=None, guide_path=None):
+        super().__init__(side=side, part=part, guide_list=guide_list, ctrl_scale=ctrl_scale, model_path=model_path, guide_path=guide_path)
+
+        self.guide_list = guide_list
+        self.gimbal = gimbal
+        self.offset = offset
+        self.pad = pad
+        self.remove_last = remove_last
+        self.fk_shape = fk_shape
+        self.gimbal_shape = gimbal_shape
+        self.offset_shape = offset_shape
+
+        if self.pad == 'auto':
+            self.pad = len(str(len(self.guide_list))) + 1
+        
+        self.create_module()
+
+
+    def create_module(self):
+        super().create_module()
+
+        self.control_rig()
+        self.output_rig()
+        self.skeleton()
+        #self.add_plugs()
+
+    def control_rig(self):
+        self.build_fk_ctrls()
+        mc.parent(self.fk_ctrls[0].top, self.control_grp)
+
+    def output_rig(self):
+        self.build_fk_chain()
+        mc.parent(self.fk_joints[0], self.module_grp)
+
+    def skeleton(self):
+        fk_chain = rChain.Chain(transform_list=self.fk_joints, side=self.side, suffix='JNT', name=self.part)
+        fk_chain.create_from_transforms(parent=self.skel)
+
+        if self.remove_last:
+            mc.delete(self.fk_ctrls[-1].top)
+            self.bind_joints = fk_chain.joints[:-1]
+        else:
+            self.bind_joints = fk_chain.joints
+
+        self.tag_bind_joints(self.bind_joints)
