bda7388bf8bca2c8de39e5ba7a34d49db1d2e25c
Jack Gashler <jgashler44@gmail.com>
2023-09-13 14:39:18 -0600

    added bone splitting functionality and twist joints

--- build/chain.py
+++ build/chain.py
@@ -118,5 +118,72 @@ class Chain:
             self.bone_lengths.append(bone_len)
 
         self.chain_length = sum(self.bone_lengths)
+
+    def split_chain(self, segments=4):
+        self.split_jnt_dict = {}
+        for bone in self.joints[:-1]:
+            split_jnts = self.split_bone(bone=bone, segments=segments)
+            self.split_jnt_dict[bone] = split_jnts
+
+    def split_bone(self, bone=None, segments=4):
+        pad = len(str(segments)) + 1
+        end_joint = mc.listRelatives(bone, children=True, type='joint')
+        s = mc.xform(bone, q=True, ws=True, t=True)
+        e = mc.xform(end_joint, q=True, ws=True, t=True)
+
+        split_jnts = []
+        for i in range(1, segments+1):
+            name_list = [bone.replace('_' + self.suffix, ''), 'seg', str(i).zfill(pad), self.suffix]
+            seg_name = '_'.join(name_list)
+            seg_joint = mc.joint(bone, name=seg_name)
+            if i > 1:
+                seg_pos = [s[axis] + ((i-1) * ((e[axis] - s[axis]) / segments)) for axis in range(3)]
+                mc.xform(seg_joint, ws=True, t=seg_pos)
+            split_jnts.append(seg_joint)
+        
+        return split_jnts
+    
+    def twist_chain(self, start_translate, start_rotate, end_translate, end_rotate, twist_bone, twist_driver, reverse=False):
+        if not mc.pluginInfo('quatNodes', q=True, loaded=True):
+            mc.loadPlugin('quatNodes')
+
+        twist_name = twist_bone.replace('_JNT', '')
+        twist_loc = mc.spaceLocator(name=twist_name + '_twist_LOC')[0]
+        driver_loc = mc.spaceLocator(name=twist_name + '_twist_driver_LOC')[0]
+        mc.hide(twist_loc, driver_loc)
+
+        mc.matchTransform(twist_loc, start_translate, position=True, rotation=False)
+        mc.matchTransform(twist_loc, start_rotate, position=False, rotation=True)
+        mc.parent(twist_loc, twist_bone)
+
+        mc.matchTransform(driver_loc, end_translate, position=True, rotation=False)
+        mc.matchTransform(driver_loc, end_rotate, position=False, rotation=True)
+        mc.parent(driver_loc, twist_driver)
+
+        mmx = mc.createNode('multMatrix', name=twist_name + '_MMX')
+        dcm = mc.createNode('decomposeMatrix', name=twist_name + '_DCM')
+        qte = mc.createNode('quatToEuler', name=twist_name + '_QTE')
+
+        mc.connectAttr(driver_loc + '.worldMatrix[0]', mmx + '.matrixIn[0]')
+        mc.connectAttr(twist_loc + '.parentInverseMatrix[0]', mmx + '.matrixIn[1]')
+        mc.connectAttr(mmx + '.matrixSum', dcm + '.inputMatrix')
+        mc.connectAttr(dcm + '.outputQuatY', qte + '.inputQuatY')
+        mc.connectAttr(dcm + '.outputQuatW', qte + '.inputQuatW')
+        mc.connectAttr(qte + '.outputRotateY', twist_loc + '.rotateY')
+        mc.setAttr(qte + '.inputRotateOrder', 1)
+
+        t_i = 1 / float(len(self.split_jnt_dict[twist_bone]))
+        t_val = t_i
+        for jnt in self.split_jnt_dict[twist_bone]:
+            t_percent = t_val - t_i
+            if reverse:
+                t_percent = 1 - t_percent
+            mdl = mc.createNode('multDoubleLinear', name=jnt + '_twist_MDL')
+            mc.connectAttr(twist_loc + '.rotateY', mdl + '.input1')
+            mc.setAttr(mdl + '.input2', t_percent)
+            mc.connectAttr(mdl + '.output', jnt + '.rotateY')
+            t_val += t_i
+
+
             
 
