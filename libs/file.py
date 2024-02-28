13df07b410987cc216e0eb90cbc67dd88aaaaea4
Jack Gashler <jgashler44@gmail.com>
2024-02-27 17:39:59 -0700

    allow parenting imported geo to something else

--- libs/file.py
+++ libs/file.py
@@ -1,7 +1,7 @@
 import maya.cmds as mc
 
 # imports a scene, finds all root nodes (no parent) and returns them
-def import_hierarchy(path, namespace='mixamorig'):      # TODO: namespace only works if set to mixamorig right now
+def import_hierarchy(path, namespace='mixamorig', parent=None):      # TODO: namespace only works if set to mixamorig right now
     mc.file(path, i=True, namespace=namespace)
     root_list = mc.ls(namespace + ':|*')                # this gets all the root nodes
     root_nodes = []
@@ -9,4 +9,7 @@ def import_hierarchy(path, namespace='mixamorig'):      # TODO: namespace only w
         root_nodes.append(root.split(':')[-1])          # this removes namespace info from the name
     mc.namespace(moveNamespace=(namespace, ':'), force=True)
     mc.namespace(removeNamespace=namespace)
+    if parent:
+        mc.parent(root_nodes, parent)
+
     return root_nodes
