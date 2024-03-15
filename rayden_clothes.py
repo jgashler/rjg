13271969e44188b1ade01490e8437169de693d1c
Jack Gashler <jgashler44@gmail.com>
2024-03-15 13:08:09 -0600

    functionalize, eliminates need for running twice

--- rayden_clothes.py
+++ rayden_clothes.py
@@ -1,19 +1,16 @@
 import maya.cmds as mc
 
-skin_src = 'RaydenNewTopo2'
+def rayden_clothes(skin_src, skin_trg_grp):
+    bind_joints = [jnt.split('.')[0] for jnt in mc.ls('*.bindJoint')]
+    geo = mc.ls(mc.select(skin_trg_grp, hierarchy=True), selection=True)
+    mc.select(skin_trg_grp, hierarchy=True)
+    geo = mc.ls(selection=True, type='mesh')
 
-skin_trg_grp = 'Ray_Extras'
+    sk_g = []
 
-bind_joints = [jnt.split('.')[0] for jnt in mc.ls('*.bindJoint')]
-geo = mc.ls(mc.select(skin_trg_grp, hierarchy=True), selection=True)
-mc.select(skin_trg_grp, hierarchy=True)
-geo = mc.ls(selection=True, type='mesh')
-
-sk_g = []
-
-for g in geo:
-    sk = mc.skinCluster(bind_joints, g, tsb=True)[0]
-    sk_g.append(sk)
-    
-for g in sk_g:
-    mc.copySkinWeights(ss='skinCluster1', ds=g, surfaceAssociation='closestPoint', noMirror=True, )
+    for g in geo:
+        sk = mc.skinCluster(bind_joints, g, tsb=True)[0]
+        sk_g.append(sk)
+        
+    for g in sk_g:
+        mc.copySkinWeights(ss='skinCluster1', ds=g, surfaceAssociation='closestPoint', noMirror=True, )
