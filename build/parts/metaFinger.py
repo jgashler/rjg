c74469dd0179637b9340897983364930a92ee230
Jack Gashler <jgashler44@gmail.com>
2024-03-21 20:22:30 -0600

    metacarpal aimVector upVector added

--- build/parts/metaFinger.py
+++ build/parts/metaFinger.py
@@ -11,7 +11,7 @@ reload(rChain)
 reload(rFk)
 
 class MetaFinger(rModule.RigModule, rFk.Fk):
-    def __init__(self, side=None, part=None, guide_list=None, ctrl_scale=1, model_path=None, guide_path=None, pad='auto', remove_last=True, fk_shape='circle', hand=None):
+    def __init__(self, side=None, part=None, guide_list=None, ctrl_scale=1, model_path=None, guide_path=None, pad='auto', remove_last=True, fk_shape='circle', hand=None, up_par=None):
         super().__init__(side=side, part=part, guide_list=guide_list, ctrl_scale=ctrl_scale, model_path=model_path, guide_path=guide_path)
         self.__dict__.update(locals())
 
@@ -42,8 +42,15 @@ class MetaFinger(rModule.RigModule, rFk.Fk):
         meta_chain = rChain.Chain(transform_list=meta_pos, side=self.side, suffix='JNT', name=self.part + 'Metacarpal')
         self.meta_chain = meta_chain.create_from_transforms(parent=self.module_grp, static=True)
 
+        up_loc = mc.spaceLocator(name=self.base_name + '_UP_LOC')[0]
+        mc.matchTransform(up_loc, self.meta_chain[0])
+        mc.xform(up_loc, t=[0, 10, 0], relative=True)
+
         mc.pointConstraint(self.fk_joints[0], self.meta_chain[1], mo=True)
-        mc.aimConstraint(self.fk_joints[0], self.meta_chain[0], mo=True)
+        mc.aimConstraint(self.fk_joints[0], self.meta_chain[0], mo=True, worldUpType=1, worldUpObject=up_loc)
+        mc.parent(up_loc, self.up_par)
+        mc.hide(up_loc)
+
 
     def skeleton(self):
         finger_chain = rChain.Chain(transform_list = [self.meta_chain[0]]+self.fk_joints, side=self.side, suffix='JNT', name=self.part)
