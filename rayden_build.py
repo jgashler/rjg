6819dc86c9cc8c8a898e2f7af0e1332679005c19
Jack Gashler <jgashler44@gmail.com>
2024-04-04 13:47:14 -0600

    improve build scripts

--- rayden_build.py
+++ rayden_build.py
@@ -1,5 +1,8 @@
 import maya.cmds as mc
+import platform
 from importlib import reload
+
+groups = 'G:' if platform.system() == 'Windows' else '/groups'
 
 import rjg.build.buildPart as rBuild
 import rjg.post.finalize as rFinal
@@ -10,47 +13,43 @@ reload(rBuild)
 reload(rFinal)
 reload(rFile)
 
-mp = '/groups/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_model.mb'
-gp = '/groups/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_guides_m.mb'
-pref = ''
+mp = groups + '/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_model_3.mb'
+gp = groups + '/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_guides_3.mb'
 body_mesh = 'Rayden_UBM'
 
 mc.file(new=True, f=True)
 
 ### BUILD SCRIPT
 root = rBuild.build_module(module_type='root', side='M', part='root', model_path=mp, guide_path=gp)
-extras = rFile.import_hierarchy('/groups/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_extras.mb', parent='MODEL')[0]
+extras = rFile.import_hierarchy(groups + '/dungeons/character/Rigging/Rigs/Rayden/ray_ubm_extras_3.mb', parent='MODEL')[0]
 mc.viewFit('perspShape', fitFactor=1, all=True, animate=True)
 
-hip = rBuild.build_module(module_type='hip', side='M', part='COG', guide_list=[pref+'Hips'], ctrl_scale=50, cog_shape='quad_arrow', waist_shape='circle')
-chest = rBuild.build_module(module_type='chest', side='M', part='chest', guide_list=[pref+'Spine2'], ctrl_scale=70, chest_shape='circle')
-spine = rBuild.build_module(module_type='spine', side='M', part='spine', guide_list=[pref+'Hips', pref+'Spine', pref+'Spine1', pref+'Spine2'], ctrl_scale=1, mid_ctrl=True)
-neck = rBuild.build_module(module_type='spine', side='M', part='neck', guide_list=[pref+'Neck', pref+'Neck1', pref+'Head'], ctrl_scale=1, mid_ctrl=False, joint_num=3)
-head = rBuild.build_module(module_type='head', side='M', part='head', guide_list=[pref+'Head'], ctrl_scale=50)
+hip = rBuild.build_module(module_type='hip', side='M', part='COG', guide_list=['Hips'], ctrl_scale=50, cog_shape='quad_arrow', waist_shape='circle')
+chest = rBuild.build_module(module_type='chest', side='M', part='chest', guide_list=['Spine2'], ctrl_scale=70, chest_shape='circle')
+spine = rBuild.build_module(module_type='spine', side='M', part='spine', guide_list=['Hips', pref+'Spine', pref+'Spine1', pref+'Spine2'], ctrl_scale=1, mid_ctrl=True)
+neck = rBuild.build_module(module_type='spine', side='M', part='neck', guide_list=['Neck', pref+'Neck1', pref+'Head'], ctrl_scale=1, mid_ctrl=False, joint_num=3)
+head = rBuild.build_module(module_type='head', side='M', part='head', guide_list=['Head'], ctrl_scale=50)
 
 
 for fs in ['Left', 'Right']:    
-    arm = rBuild.build_module(module_type='biped_limb', side=fs[0], part='arm', guide_list=[pref + fs + piece for piece in ['Arm', 'ForeArm', 'Hand']], offset_pv=50, ctrl_scale=5)
-    clavicle = rBuild.build_module(module_type='clavicle', side=fs[0], part='clavicle', guide_list=[pref + fs + piece for piece in ['Shoulder', 'Arm']], local_orient=False, ctrl_scale=9) 
-    hand = rBuild.build_module(module_type='hand', side=fs[0], part='hand', guide_list=[pref + fs + 'Hand'], ctrl_scale=8)
+    arm = rBuild.build_module(module_type='biped_limb', side=fs[0], part='arm', guide_list=[fs + piece for piece in ['Arm', 'ForeArm', 'Hand']], offset_pv=50, ctrl_scale=5)
+    clavicle = rBuild.build_module(module_type='clavicle', side=fs[0], part='clavicle', guide_list=[fs + piece for piece in ['Shoulder', 'Arm']], local_orient=False, ctrl_scale=9) 
+    hand = rBuild.build_module(module_type='hand', side=fs[0], part='hand', guide_list=[fs + 'Hand'], ctrl_scale=8)
     
-    leg = rBuild.build_module(module_type='biped_limb', side=fs[0], part='leg', guide_list=[pref + fs + piece for piece in ['UpLeg', 'Leg', 'Foot']], offset_pv=50, ctrl_scale=8)
-    foot = rBuild.build_module(module_type='foot', side=fs[0], part='foot', guide_list=[pref + fs + piece for piece in ['Foot', 'ToeBase', 'Toe_End']], ctrl_scale=10, toe_piv=pref + fs+'ToePiv', heel_piv=pref + fs+'HeelPiv', in_piv=pref + fs+'In', out_piv=pref + fs+'Out')
+    leg = rBuild.build_module(module_type='biped_limb', side=fs[0], part='leg', guide_list=[fs + piece for piece in ['UpLeg', 'Leg', 'Foot']], offset_pv=50, ctrl_scale=8)
+    foot = rBuild.build_module(module_type='foot', side=fs[0], part='foot', guide_list=[fs + piece for piece in ['Foot', 'ToeBase', 'Toe_End']], ctrl_scale=10, toe_piv=pref + fs+'ToePiv', heel_piv=pref + fs+'HeelPiv', in_piv=pref + fs+'In', out_piv=pref + fs+'Out')
     
     fingers = []
-    #for f in ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']:
-    #    finger = rBuild.build_module(module_type='meta_finger', side=fs[0], part='finger'+f, guide_list=[pref + fs + 'Hand' + f + str(num+1) for num in range(4)], ctrl_scale=1, hand=fs + 'Hand', up_par='arm_{}_03_fk_CTRL'.format(fs[0]))
-    #    fingers.append(finger)
         
     for f in ['Index', 'Middle', 'Ring', 'Pinky']:
-        finger = rBuild.build_module(module_type='finger', side=fs[0], part='finger'+f, guide_list=[pref + fs + 'Hand' + f + str(num) for num in range(5)], ctrl_scale=1, fk_shape='lollipop')
+        finger = rBuild.build_module(module_type='finger', side=fs[0], part='finger'+f, guide_list=[fs + 'Hand' + f + str(num) for num in range(5)], ctrl_scale=1, fk_shape='lollipop')
         fingers.append(finger)
-    thumb = rBuild.build_module(module_type='finger', side=fs[0], part='fingerThumb', guide_list=[pref + fs + 'HandThumb' + str(num+1) for num in range(4)], ctrl_scale=1, fk_shape='lollipop')
+    thumb = rBuild.build_module(module_type='finger', side=fs[0], part='fingerThumb', guide_list=[fs + 'HandThumb' + str(num+1) for num in range(4)], ctrl_scale=1, fk_shape='lollipop')
     fingers.append(thumb)    
     
 
 rFinal.final(utX=90, utY=0, DutZ=15, utScale=3)
-mc.delete('Ray_Guides_Meta')
+mc.delete('Hips')
 
 ### DEFAULT SKIN
 
@@ -70,8 +69,10 @@ reload(rCtrlIO)
 
 print("Reading skin weight files...")
 
-rCtrlIO.read_ctrls("/groups/dungeons/character/Rigging/Rigs/Rayden/Controls", curve_file='rayden_control_curves')
-rWeightNgIO.read_skin(body_mesh, '/groups/dungeons/character/Rigging/Rigs/Rayden/Skin', 'rayden_ubm_rough') 
+rCtrlIO.read_ctrls(groups + "/dungeons/character/Rigging/Rigs/Rayden/Controls", curve_file='rayden_control_curves')
+rWeightNgIO.read_skin(body_mesh, groups + '/dungeons/character/Rigging/Rigs/Rayden/Skin', 'Dungeons_UBM_V1') 
+
+mc.copySkinWeights(ss='skinCluster1', ds='skinCluster1', mm='YZ', sa='closestPoint', ia='closestJoint') # necessary??
 
 import rjg.rayden_clothes as rc
 reload(rc)
@@ -79,15 +80,34 @@ rc.rayden_clothes(body_mesh, extras)
 
 ##### PROJECT FACE
 
-face = rFile.import_hierarchy('/groups/dungeons/anim/rigs/RaydenFace.mb')
+face = rFile.import_hierarchy(groups + '/dungeons/anim/Rigs/RaydenFace.mb')
 import rjg.post.faceProject as rFaceProj
 reload(rFaceProj)
 rFaceProj.project(body=body_mesh, f_model='FaceAtOrigin', f_rig='face_M', f_skel='faceRoot_JNT')
-mc.delete(face)
+#mc.delete(face)
 
 
 mc.select(clear=True)
 print("Rayden rig build complete.")
 
 #rCtrlIO.write_ctrls("/groups/dungeons/character/Rigging/Rigs/Rayden/Controls", force=True, name='rayden_control_curves')
-#rWeightIO.write_skin("/groups/dungeons/character/Rigging/Rigs/Rayden/Skin", force=True, name='rayden_skin_weights')
+# Don't use this. Use export skin weights from ngskintools. rWeightIO.write_skin("/groups/dungeons/character/Rigging/Rigs/Rayden/Skin", force=True, name='rayden_skin_weights')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- robin_build.py
+++ robin_build.py
@@ -1,5 +1,8 @@
 import maya.cmds as mc
+import platform
 from importlib import reload
+
+groups = 'G:' if platform.system() == 'Windows' else '/groups'
 
 import rjg.build.buildPart as rBuild
 import rjg.post.finalize as rFinal
@@ -10,42 +13,40 @@ reload(rBuild)
 reload(rFinal)
 reload(rFile)
 
-mp = '/groups/dungeons/character/Rigging/Robin/robin_model.mb'
-gp = '/groups/dungeons/character/Rigging/Robin/robin_guides.mb'
-pref = ''
+mp = groups + '/dungeons/character/Rigging/Rigs/Robin/robin_ubm_model.mb'
+gp = groups + '/dungeons/character/Rigging/Rigs/Robin/robin_ubm_guides.mb'
+body_mesh = 'Robin_UBM'
 
 mc.file(new=True, f=True)
 
 ### BUILD SCRIPT
 root = rBuild.build_module(module_type='root', side='M', part='root', model_path=mp, guide_path=gp)
-rFile.import_hierarchy('/groups/dungeons/character/Rigging/Robin/robin_extras.mb', parent='MODEL')
+extras = rFile.import_hierarchy(groups + '/dungeons/character/Rigging/Rigs/Robin/robin_ubm_extras.mb', parent='MODEL')[0]
 mc.viewFit('perspShape', fitFactor=1, all=True, animate=True)
 
-hip = rBuild.build_module(module_type='hip', side='M', part='COG', guide_list=[pref+'Hips'], ctrl_scale=50, cog_shape='quad_arrow', waist_shape='circle')
-chest = rBuild.build_module(module_type='chest', side='M', part='chest', guide_list=[pref+'Spine2'], ctrl_scale=70, chest_shape='circle')
-spine = rBuild.build_module(module_type='spine', side='M', part='spine', guide_list=[pref+'Hips', pref+'Spine', pref+'Spine1', pref+'Spine2'], ctrl_scale=1, mid_ctrl=True)
-neck = rBuild.build_module(module_type='neck', side='M', part='neck', guide_list=[pref+'Neck', pref+'Neck1', pref+'Head'], ctrl_scale=1, mid_ctrl=True)
-head = rBuild.build_module(module_type='head', side='M', part='head', guide_list=[pref+'Head'], ctrl_scale=50)
+hip = rBuild.build_module(module_type='hip', side='M', part='COG', guide_list=['Hips'], ctrl_scale=50, cog_shape='quad_arrow', waist_shape='circle')
+chest = rBuild.build_module(module_type='chest', side='M', part='chest', guide_list=['Spine2'], ctrl_scale=70, chest_shape='circle')
+spine = rBuild.build_module(module_type='spine', side='M', part='spine', guide_list=['Hips', pref+'Spine', pref+'Spine1', pref+'Spine2'], ctrl_scale=1, mid_ctrl=True)
+neck = rBuild.build_module(module_type='spine', side='M', part='neck', guide_list=['Neck', pref+'Neck1', pref+'Head'], ctrl_scale=1, mid_ctrl=False, joint_num=3)
+head = rBuild.build_module(module_type='head', side='M', part='head', guide_list=['Head'], ctrl_scale=50)
 
 
 for fs in ['Left', 'Right']:    
-    arm = rBuild.build_module(module_type='biped_limb', side=fs[0], part='arm', guide_list=[pref + fs + piece for piece in ['Arm', 'ForeArm', 'Hand']], offset_pv=50, ctrl_scale=5)
-    clavicle = rBuild.build_module(module_type='clavicle', side=fs[0], part='clavicle', guide_list=[pref + fs + piece for piece in ['Shoulder', 'Arm']], local_orient=False, ctrl_scale=9) 
-    hand = rBuild.build_module(module_type='hand', side=fs[0], part='hand', guide_list=[pref + fs + 'Hand'], ctrl_scale=8)
+    arm = rBuild.build_module(module_type='biped_limb', side=fs[0], part='arm', guide_list=[fs + piece for piece in ['Arm', 'ForeArm', 'Hand']], offset_pv=50, ctrl_scale=5)
+    clavicle = rBuild.build_module(module_type='clavicle', side=fs[0], part='clavicle', guide_list=[fs + piece for piece in ['Shoulder', 'Arm']], local_orient=False, ctrl_scale=9) 
+    hand = rBuild.build_module(module_type='hand', side=fs[0], part='hand', guide_list=[fs + 'Hand'], ctrl_scale=8)
     
-    leg = rBuild.build_module(module_type='biped_limb', side=fs[0], part='leg', guide_list=[pref + fs + piece for piece in ['UpLeg', 'Leg', 'Foot']], offset_pv=50, ctrl_scale=8)
-    foot = rBuild.build_module(module_type='foot', side=fs[0], part='foot', guide_list=[pref + fs + piece for piece in ['Foot', 'ToeBase', 'Toe_End']], ctrl_scale=10, toe_piv=pref + fs+'ToePiv', heel_piv=pref + fs+'HeelPiv', in_piv=pref + fs+'In', out_piv=pref + fs+'Out')
+    leg = rBuild.build_module(module_type='biped_limb', side=fs[0], part='leg', guide_list=[fs + piece for piece in ['UpLeg', 'Leg', 'Foot']], offset_pv=50, ctrl_scale=8)
+    foot = rBuild.build_module(module_type='foot', side=fs[0], part='foot', guide_list=[fs + piece for piece in ['Foot', 'ToeBase', 'Toe_End']], ctrl_scale=10, toe_piv=pref + fs+'ToePiv', heel_piv=pref + fs+'HeelPiv', in_piv=pref + fs+'In', out_piv=pref + fs+'Out')
     
     fingers = []
-    for f in ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']:
-        #finger = rBuild.build_module(module_type='finger', side=fs[0], part='finger'+f, guide_list=[fs + 'Hand' + f + str(num+1) for num in range(4)], ctrl_scale=3)
-        #finger = rBuild.build_module(module_type='biped_limb', side=fs[0], part='finger'+f, guide_list=[fs + 'Hand' + f + str(num+1) for num in range(4)], ctrl_scale=2.5, offset_pv=10, bendy=False, twisty=False, segments=False, gimbal=False, offset=False)
-        finger = rBuild.build_module(module_type='meta_finger', side=fs[0], part='finger'+f, guide_list=[pref + fs + 'Hand' + f + str(num+1) for num in range(4)], ctrl_scale=2, hand=fs + 'Hand')
+        
+    for f in ['Index', 'Middle', 'Ring', 'Pinky']:
+        finger = rBuild.build_module(module_type='finger', side=fs[0], part='finger'+f, guide_list=[fs + 'Hand' + f + str(num) for num in range(5)], ctrl_scale=1, fk_shape='lollipop')
         fingers.append(finger)
-
-
-
-#####
+    thumb = rBuild.build_module(module_type='finger', side=fs[0], part='fingerThumb', guide_list=[fs + 'HandThumb' + str(num+1) for num in range(4)], ctrl_scale=1, fk_shape='lollipop')
+    fingers.append(thumb)    
+    
 
 rFinal.final(utX=90, utY=0, DutZ=15, utScale=3)
 mc.delete('Hips')
@@ -53,26 +54,60 @@ mc.delete('Hips')
 ### DEFAULT SKIN
 
 bind_joints = [jnt.split('.')[0] for jnt in mc.ls('*.bindJoint')]
-geo = mc.ls('Robin_Retopologized')
+geo = mc.ls(body_mesh)
 for g in geo:
-    mc.skinCluster(bind_joints, g, tsb=True, bindMethod=2)
-    #mc.geomBind(bm=3, 
+    mc.skinCluster(bind_joints, g, tsb=True, bindMethod=0)
 
 ### SKIN/CURVE IO
 
+import rjg.post.dataIO.ng_weights as rWeightNgIO
 import rjg.post.dataIO.weights as rWeightIO
 import rjg.post.dataIO.controls as rCtrlIO
+reload(rWeightNgIO)
 reload(rWeightIO)
 reload(rCtrlIO)
 
-rCtrlIO.read_ctrls("/groups/dungeons/character/Rigging/Robin/Controls", curve_file='robin_control_curves')
-#rCtrlIO.write_ctrls("/groups/dungeons/character/Rigging/Robin/Controls", force=True, name='robin_control_curves')
-
-#rWeightIO.write_skin("/groups/dungeons/character/Rigging/Robin/Skin", force=True, name='robin_skin_weights')
 print("Reading skin weight files...")
-rWeightIO.read_skin("/groups/dungeons/character/Rigging/Robin/Skin", weights_file='robin_skin_weights')
-import rjg.robin_clothes as rc
+
+rCtrlIO.read_ctrls(groups + "/dungeons/character/Rigging/Rigs/Rayden/Controls", curve_file='rayden_control_curves')
+rWeightNgIO.read_skin(body_mesh, groups + '/dungeons/character/Rigging/Rigs/Rayden/Skin', 'Dungeons_UBM_V1') 
+
+mc.copySkinWeights(ss='skinCluster1', ds='skinCluster1', mm='YZ', sa='closestPoint', ia='closestJoint') # necessary??
+
+import rjg.rayden_clothes as rc
 reload(rc)
+rc.rayden_clothes(body_mesh, extras)
+
+##### PROJECT FACE
+'''
+face = rFile.import_hierarchy(groups + '/dungeons/anim/Rigs/RaydenFace.mb')
+import rjg.post.faceProject as rFaceProj
+reload(rFaceProj)
+rFaceProj.project(body=body_mesh, f_model='FaceAtOrigin', f_rig='face_M', f_skel='faceRoot_JNT')
+mc.delete(face)
+'''
 
 mc.select(clear=True)
 print("Robin rig build complete.")
+
+#rCtrlIO.write_ctrls("/groups/dungeons/character/Rigging/Rigs/Rayden/Controls", force=True, name='rayden_control_curves')
+# Don't use this. Use export skin weights from ngskintools. rWeightIO.write_skin("/groups/dungeons/character/Rigging/Rigs/Rayden/Skin", force=True, name='rayden_skin_weights')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
