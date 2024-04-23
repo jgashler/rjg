b2e1819ab05511ba9caac19dda31106da0cd1579
Jack Gashler <jgashler44@gmail.com>
2024-04-23 11:52:50 -0600

    handle test cases

--- libs/file.py
+++ libs/file.py
@@ -2,6 +2,8 @@ import maya.cmds as mc
 
 # imports a scene, finds all root nodes (no parent) and returns them
 def import_hierarchy(path, namespace='default', parent=None):
+    if not path:
+        return
     mc.file(path, i=True, namespace=namespace)
     root_list = mc.ls(namespace + ':|*')                # this gets all the root nodes
     root_nodes = []
